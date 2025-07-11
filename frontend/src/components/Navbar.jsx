b626b150ad806a193bac3ca68d43e12730054d76
UnknownRG <rajgayakwad01@gmail.com>
2025-07-11 19:32:37 +0530

    update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-import {NavLink} from 'react-router-dom'
+import {NavLink ,useNavigate} from 'react-router-dom'
 const Navbar = () => {
+    const navigate = useNavigate();
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img
@@ -10,23 +11,25 @@ const Navbar = () => {
         <ul className='hidden md:flex item-start gap-5 font-medium'>
             <NavLink to='/'>
                 <li  className='py-1'>HOME</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+                <hr className='border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden'/>
             </NavLink>
             <NavLink to='/doctors' >
                 <li className='py-1'>ALL DOCTORS</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+                <hr className='border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden'/>
             </NavLink>
             <NavLink to='/about'>
                 <li className='py-1'>ABOUT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+                <hr className='border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden'/>
             </NavLink>
             <NavLink to='/contact'>
                 <li className='py-1'>CONTACT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+                <hr className='border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden'/>
             </NavLink>
         </ul>
-        <div>
-            <button>Create account</button>
+        <div className='flex items-center gap-4'>
+            <button
+             onClick={() => navigate('/login')}
+             className='bg-[#5f6FFF] text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
         </div>
     </div>
   )
