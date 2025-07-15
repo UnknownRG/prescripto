aeca92aae93141541478221523776a4cdd958d82
UnknownRG <rajgayakwad01@gmail.com>
2025-07-15 16:46:34 +0530

    add Navbar with logout functionality and dynamic role badge based on auth token for admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
           const {data} = await axios.post(backendUrl + '/api/admin/login',{email,password})
           if(data.success){
             localStorage.setItem('aToken',data.token)
-            console.log(data.token)
+            setAToken(data.token)
           }else{
             toast.error(data.message)
           }
