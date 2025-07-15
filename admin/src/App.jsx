059645bc9949f44c55245573208dc94ac422029d
UnknownRG <rajgayakwad01@gmail.com>
2025-07-15 16:18:18 +0530

    create context for admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  
+
   return (
-    <div >App</div>
+    <div >
+    <Login />
+    <ToastContainer />
+    </div>
   )
 }
 
