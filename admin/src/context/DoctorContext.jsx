05d5b3e34bbb53f30f32c4c073b9af23a1410f7f
UnknownRG <rajgayakwad01@gmail.com>
2025-07-18 14:18:43 +0530

    Add doctor dashboard API
    - Created `doctorDashboard` controller to return dashboard stats:
      - Total earnings (placeholder)
      - Appointment and patient counts
      - Latest 5 appointments

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -11,6 +11,8 @@ const DoctorContextProvider = (props) => {
 
     const [dToken,setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):'')
     const [appointments,setAppointments] = useState([])
+    const [dashData,setDashData] = useState(false)
+
 
     const getAppointments = async () => {
         try {
@@ -73,13 +75,29 @@ const cancelAppointment = async (appointmentId) => {
 
 }
 
+const getDashData = async () => {
+  try {
+    const {data} = await axios.get(backendUrl + '/api/doctor/dashboard', {headers : {dToken}})
+    if (data.success) {
+      setDashData(data.dashData)
+      // console.log(data.dashData)
+    }else{
+      toast.error(data.message)
+    }
+  } catch (error) {
+    console.log(error);
+        toast.error(error.message)
+  }
+}
+
     const value = {
         dToken,setDToken,
         backendUrl,
         appointments,setAppointments,
         getAppointments,
         completeAppointment,
-        cancelAppointment
+        cancelAppointment,
+        dashData,setDashData,getDashData
 
     }
 
